ea3571b3feb230a2fb08f06fccc06d658459ceb3
Archer <421228286@qq.com>
2016-04-07 21:09:01 +0800

    NetWorkImageView的用法

--- app/src/main/java/com/example/archer/myvolleyimage/MainActivity.java
+++ app/src/main/java/com/example/archer/myvolleyimage/MainActivity.java
@@ -4,26 +4,41 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.LruCache;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private Button button;
+    private NetworkImageView networkImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadImageVolley();
+                NetWorkImageViewVolley();
+            }
+        });
+
     }
 
     public void  init(){
         imageView= (ImageView) findViewById(R.id.imageView);
-        LoadImageVolley();
+        networkImageView= (NetworkImageView) findViewById(R.id.imageView2);
+
+         button= (Button) findViewById(R.id.B1);
     }
 
     public void LoadImageVolley(){
@@ -49,5 +64,27 @@ public class MainActivity extends AppCompatActivity {
 
         ImageLoader.ImageListener listener= ImageLoader.getImageListener(imageView, R.mipmap.ic_launcher, R.mipmap.ic_launcher);
         imageLoader.get(imageUrl,listener);
+    }
+
+    public void NetWorkImageViewVolley(){
+        String imageUrl="http://img.my.csdn.net/uploads/201404/13/1397393290_5765.jpeg";
+        RequestQueue requestQueue=Volley.newRequestQueue(this);
+        final  LruCache<String,Bitmap> lruCache=new LruCache<>(20);
+        ImageLoader.ImageCache imageCache=new ImageLoader.ImageCache() {
+            @Override
+            public Bitmap getBitmap(String s) {
+                return lruCache.get(s);
+            }
+
+            @Override
+            public void putBitmap(String s, Bitmap bitmap) {
+               lruCache.put(s,bitmap);
+            }
+        };
+
+        ImageLoader imageLoader=new ImageLoader(requestQueue,imageCache);
+        networkImageView.setTag("url");
+        networkImageView.setImageUrl(imageUrl,imageLoader);
+
     }
 }
